dc9d1f61c877648c69492ff864f284b5a10463c0
sanjaypalkar <sanjayitech06@gmail.com>
2026-06-04 17:47:31 +0530

    Updated project

--- script.js
+++ script.js
@@ -1,6 +1,51 @@
-let count = 0;
+let cart = [];
 
-function addToCart() {
-    count++;
-    document.getElementById("cart-count").innerText = count;
+function addToCart(name, price){
+
+    cart.push({
+        name:name,
+        price:price
+    });
+
+    displayCart();
+}
+
+function displayCart(){
+
+    let cartDiv = document.getElementById("cart");
+
+    let html = "";
+
+    let total = 0;
+
+    for(let i=0;i<cart.length;i++){
+
+        html += `
+        <p>
+        ${cart[i].name} - ₹${cart[i].price}
+        <button onclick="removeItem(${i})">Remove</button>
+        </p>
+        `;
+
+        total += cart[i].price;
+    }
+
+    cartDiv.innerHTML = html;
+
+    document.getElementById("total").innerHTML =
+    "Total: ₹" + total;
+}
+
+function removeItem(index){
+
+    cart.splice(index,1);
+
+    displayCart();
+}
+
+function clearCart(){
+
+    cart = [];
+
+    displayCart();
 }
